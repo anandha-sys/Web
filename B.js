060ac1f80c9c2f107821973a8020aaa31cdc1ef0
anandha-sys <krishnanhanandha45@gmail.com>
2026-06-09 09:00:53 +0530

    Add mouse movement effects to card elements

---
+++ B.js
@@ -0,0 +1,30 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+
+card.addEventListener('mousemove',(e)=>{
+
+const rect = card.getBoundingClientRect();
+
+const x = e.clientX - rect.left;
+const y = e.clientY - rect.top;
+
+const rotateY = ((x/rect.width)-0.5)*20;
+const rotateX = ((y/rect.height)-0.5)*-20;
+
+card.style.transform =
+`perspective(1000px)
+rotateX(${rotateX}deg)
+rotateY(${rotateY}deg)
+scale(1.04)`;
+
+});
+
+card.addEventListener('mouseleave',()=>{
+
+card.style.transform =
+'perspective(1000px) rotateX(0) rotateY(0)';
+
+});
+
+});
